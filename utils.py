9e949a2b0a78b455badb327c4b3f190a99e8000d
Jonne Goedhart <jonnegoedhart@gmail.com>
2021-11-04 14:57:59 +0100

    m

---
+++ utils.py
@@ -0,0 +1,57 @@
+import numpy as np
+import numpy.polynomial.polynomial as poly
+
+from scipy import ndimage
+
+
+
+def unit_vector_norm(X):
+    return (X.T / np.sqrt((X**2).sum(axis=1))).T
+
+def split_Raman_af(X, wavelength):
+    """
+    Removing spikes from the data to extract the autofluorescence.
+    This is done by applying smoothing filter to the data and then taking the min of the smoothing filter and original data.
+    """
+    a = X
+    c = 50
+
+    # remove the top of the spikes from data, by using a Gaussian smoothing filter
+    for _ in range(5):      
+        a[:,c] = X[:,c]
+        a[:,-c] = X[:,-c]      
+        a1 = ndimage.gaussian_filter(a, (0, 30), mode='nearest')
+        a = np.min([a, a1], axis=0)
+
+    # remove the spikes from data, by using a polynominal fit
+    for _ in range(5):
+        a[:,c] = X[:,c]
+        a[:,-c] = X[:,-c]        
+        z = poly.polyfit(wavelength[::5], a[:,::5].T, 5)
+        a1 = poly.polyval(wavelength, z)
+        a = np.min([a, a1], axis=0)
+        
+    # smooth the curve the data, (to remove remnants of noise in the photoluminescence signal)
+    for _ in range(10):           
+        a[:,1] = X[:,1]
+        a[:,-1] = X[:,-1]         
+        a = ndimage.gaussian_filter(a, (0, 10), mode='nearest')
+
+    # make the Raman signal non-negative, (to remove remnants of noise in the Raman signal)
+    return (X-a).clip(min=0), a 
+
+def smoothing(X, smooth=5, transition=10, spike_width=7):
+    """
+    Only remove noise from low noise to signal area's to maintain the intensity of the spikes.
+    Noise is removed with a gaussian filter in spectral dimension.
+    """
+    grad = ndimage.gaussian_filter(X, (0, 1), order=1)
+    grad_abs = np.abs(grad)
+    grad_abs_sm = ndimage.gaussian_filter(grad_abs, (0, 5))
+    mean_grad = np.mean(grad_abs, 1) + 1 / np.std(grad_abs, 1) * 3
+    
+    spikes = ((grad_abs_sm.T > mean_grad ).astype(float)).T 
+    spikes = np.round(ndimage.gaussian_filter(spikes, (0, spike_width)))
+    spikes = ndimage.uniform_filter(spikes, (0, transition))
+    
+    return (1 - spikes) * ndimage.gaussian_filter(X, (0,smooth)) + spikes * X
