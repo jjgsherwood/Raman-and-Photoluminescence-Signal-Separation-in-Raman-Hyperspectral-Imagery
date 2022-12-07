1f525097c5eb8e3bed57f1ba63754b5bd02b6060
Jonne Goedhart <jonnegoedhart@gmail.com>
2022-12-07 12:52:52 +0100

    m

--- utils/dataset_utils.py
+++ utils/dataset_utils.py
@@ -20,13 +20,13 @@ class Vector_unit_normalization():
 class RandomFlip():
     def __init__(self):
         self.rng = np.random.default_rng()
-        
+
     def __call__(self, x):
         return torch.flip(x, tuple(self.rng.choice([-3,-2], size=self.rng.integers(3), replace=False)))
 
     def __repr__(self):
         return "Random horizontal flip and/or vertical flip"
-        
+
 class Random_Rotate_90():
     def __call__(self, x):
         return torch.rot90(x, random.randint(0,3), [-3, -2])
@@ -62,45 +62,45 @@ class TensorDataset(Dataset):
     def __len__(self):
         return self.data.size(0)*self.patches_per_images
 
-def split_data(dataset, test_size=TEST_RATIO, seed=42):
-    """ Something might be wrong here """
-    np.random.seed(seed)
-    train, test = train_test_split(dataset.data, test_size=test_size)
-    train_dataset = copy.copy(dataset)
-    test_dataset = copy.copy(dataset)
+# def split_data(dataset, test_size=TEST_RATIO, seed=42):
+#     """ Something might be wrong here """
+#     np.random.seed(seed)
+#     train, test = train_test_split(dataset.data, test_size=test_size)
+#     train_dataset = copy.copy(dataset)
+#     test_dataset = copy.copy(dataset)
+#
+#     train_dataset.data = train
+#     test_dataset.data = test
+#     return train_dataset, test_dataset
 
-    train_dataset.data = train
-    test_dataset.data = test
-    return train_dataset, test_dataset
-
-def load_liver(data, batch_size, sample_size=5):
-    transform = transforms.Compose([
-        Vector_unit_normalization(),
-        RandomFlip(),
-        Random_Rotate_90(),
-    ])
-
-    # tensor_data = torch.Tensor(data)
-    dataset = TensorDataset(data, transform, sample_size)
-
-    # test data and train data are split on image level
-    # Thus a whole images is either test data or train data
-    train_dataset, test_dataset = split_data(dataset)
-
-    train_loader = DataLoader(train_dataset, batch_size, shuffle=True,
-                              drop_last=False, pin_memory=True, num_workers=NUM_WORKERS)
-    test_loader = DataLoader(test_dataset, batch_size, shuffle=False,
-                             drop_last=False, pin_memory=True, num_workers=NUM_WORKERS)
-
-    return train_loader, test_loader
-
-def load_liver_all(data):
-    transform = transforms.Compose([
-        Vector_unit_normalization(),
-    ])
-
-    data = create_dataset_1x1(data)
-    return torch.Tensor(data)
+# def load_liver(data, batch_size, sample_size=5):
+#     transform = transforms.Compose([
+#         Vector_unit_normalization(),
+#         RandomFlip(),
+#         Random_Rotate_90(),
+#     ])
+#
+#     # tensor_data = torch.Tensor(data)
+#     dataset = TensorDataset(data, transform, sample_size)
+#
+#     # test data and train data are split on image level
+#     # Thus a whole images is either test data or train data
+#     train_dataset, test_dataset = split_data(dataset)
+#
+#     train_loader = DataLoader(train_dataset, batch_size, shuffle=True,
+#                               drop_last=False, pin_memory=True, num_workers=NUM_WORKERS)
+#     test_loader = DataLoader(test_dataset, batch_size, shuffle=False,
+#                              drop_last=False, pin_memory=True, num_workers=NUM_WORKERS)
+#
+#     return train_loader, test_loader
+#
+# def load_liver_all(data):
+#     transform = transforms.Compose([
+#         Vector_unit_normalization(),
+#     ])
+#
+#     data = create_dataset_1x1(data)
+#     return torch.Tensor(data)
 
 def split_data_per_image(dataset, test_size=TEST_RATIO):
     n_images = dataset.data.shape[0]
@@ -113,18 +113,24 @@ def split_data_per_image(dataset, test_size=TEST_RATIO):
     test_dataset.split(indices)
     return train_dataset, test_dataset
 
-def load_splitdata(data, batch_size):
+def load_splitdata(data, batch_size, test_size=TEST_RATIO):
     # tensor_data = torch.Tensor(data)
     dataset = SplitDataset(data)
 
     # test data and train data are split on image level
     # Thus a whole images is either test data or train data
-    train_dataset, test_dataset = split_data_per_image(dataset)
+    if test_size is not None:
+        train_dataset, test_dataset = split_data_per_image(dataset, test_size)
+    else:
+        train_dataset = dataset
 
     train_loader = DataLoader(train_dataset, batch_size, shuffle=True,
                               drop_last=False, pin_memory=True, num_workers=NUM_WORKERS)
-    test_loader = DataLoader(test_dataset, batch_size, shuffle=False,
-                             drop_last=False, pin_memory=True, num_workers=NUM_WORKERS)
+    if test_size is not None:
+        test_loader = DataLoader(test_dataset, batch_size, shuffle=False,
+                                 drop_last=False, pin_memory=True, num_workers=NUM_WORKERS)
+    else:
+        test_loader = None
 
     return train_loader, test_loader
 
@@ -139,7 +145,7 @@ class SplitDataset(Dataset):
         self.raman = torch.Tensor(np.array(self.raman))
         self.photo = torch.Tensor(np.array(self.photo))
         self.transform = transform
-        
+
     def split(self, indices):
         self.data = self.data[indices]
         self.raman = self.raman[indices]
@@ -151,8 +157,8 @@ class SplitDataset(Dataset):
         index_x = pixel % self.data.size(1)
         index_y = pixel // self.data.size(1)
         index_image = index // (self.data.size(1) * self.data.size(2))
-        data, raman, photo = (self.data[index_image, index_x, index_y, :], 
-                             self.raman[index_image, index_x, index_y, :], 
+        data, raman, photo = (self.data[index_image, index_x, index_y, :],
+                             self.raman[index_image, index_x, index_y, :],
                              self.photo[index_image, index_x, index_y, :])
         if self.transform:
             data, raman, photo = self.transform(data), self.transform(raman), self.transform(photo)
--- utils/train_utils.py
+++ utils/train_utils.py
@@ -20,16 +20,16 @@ def train(model, optimizer, loader, loss_func, acc_func, log_step=None, device=t
 
     for batch_idx, (x, *y) in enumerate(loader):
         optimizer.zero_grad()
-        
-        x = x.to(device)      
+
+        x = x.to(device)
         y_ = model(x)
 
         loss = loss_func(y, y_, x)
 
         loss.backward()
-        
+
         torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
-        
+
         optimizer.step()
 
         if log_step:
@@ -47,7 +47,7 @@ def train(model, optimizer, loader, loss_func, acc_func, log_step=None, device=t
 #                         for f in data.reshape(-1,data.shape[-1]):
 #                             plt.plot(f)
                         plt.show()
-                
+
                 if SHOW_GRADS:
                     for layer in model.net:
                         try:
@@ -58,15 +58,13 @@ def train(model, optimizer, loader, loss_func, acc_func, log_step=None, device=t
                         data = layer.weight.grad.detach().numpy().flatten()
                         plt.hist(data, 50, density=True)
                         plt.show()
-                        
 
-    
                 print('  {}| {:5d}/{:5d}| bits: {:2.6f}'.format(
                     datetime.now().strftime('%Y-%m-%d %H:%M:%S'), batch_idx,
                     len(loader), acc_func(y, y_, x) / torch.mean(torch.sum(x, 1))
                 ), flush=True)
 
-def test(model, loader, loss_func, acc_func, device=torch.device('cuda')):
+def test(model, loader, loss_func, acc_func, log_step, device=torch.device('cuda')):
     model.eval()
     acc = []
 
@@ -75,12 +73,13 @@ def test(model, loader, loss_func, acc_func, device=torch.device('cuda')):
         for batch_idx, (x, *y) in enumerate(loader):
             x = x.to(device)
             y_ = model(x)
-            loss = acc_func(y, y_, x) / torch.mean(torch.sum(x, 1))
-#             print('{}| Validation {:5d}/{:5d}| bits: {:2.6f}'.format(
-#                 datetime.now().strftime('%Y-%m-%d %H:%M:%S'), batch_idx,
-#                 len(loader), loss
-#             ), flush=True)
-            
+            loss = loss_func(y, y_, x) / torch.mean(torch.sum(x, 1))
+            if log_step:
+                if batch_idx % log_step == 0:
+                    print('  {}| {:5d}/{:5d}| bits: {:2.6f}'.format(
+                        datetime.now().strftime('%Y-%m-%d %H:%M:%S'), batch_idx,
+                        len(loader), acc_func(y, y_, x, data="Validation") / torch.mean(torch.sum(x, 1))
+                    ), flush=True)
+
             tmp.append(loss.cpu().detach().numpy())
         print(f"Validation average loss: {np.mean(tmp)}")
-            
