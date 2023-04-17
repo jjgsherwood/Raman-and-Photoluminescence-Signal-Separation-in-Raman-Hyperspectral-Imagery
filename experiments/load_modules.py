1ba11449810ce0bce510f68ea176810f653c004e
Jonne Goedhart <jonnegoedhart@gmail.com>
2023-04-17 18:23:53 +0200

    m

--- experiments/load_modules.py
+++ experiments/load_modules.py
@@ -10,7 +10,7 @@ from scipy.fft import dct
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from signal_processing import *
-from GUI.Process import load_files
+from GUI.Process import load_files, save_data
 
 from IPython.display import display, HTML
 display(HTML("<style>.container { width:98% !important; }</style>"))
